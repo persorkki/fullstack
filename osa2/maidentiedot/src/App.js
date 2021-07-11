1ca1795f51d64cbb86c0c2d2c01509e377a636ef
Persorkki <persorkki@protonmail.com>
2021-07-11 18:18:38 +0300

    2.13* maiden tiedot, step2 heikko ratkaisu toistaiseksi

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -8,10 +8,24 @@ const FindCountries = ({find, handleInputChange}) => {
     </div>
   )
 }
-
+const FullCountryInfo = ({country}) => {
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h3>languages</h3>
+      <ul>
+        {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+      </ul>
+      <img alt="flag" width="100" src={country.flag}/>
+    </div>
+  )
+}
 //TODO: split into smaller components
-const ListCountries = ({ countries, filter }) => {
+const ListCountries = ({ countries, filter, setFilter }) => {
   const filtered = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
+
   if (filtered.length > 10) {
     return (
       <div>Too many matches, specify another filter</div>
@@ -20,22 +34,19 @@ const ListCountries = ({ countries, filter }) => {
   if (filtered.length === 1) {
     const country = filtered[0]
     return (
-      <div>
-        <h1>{country.name}</h1>
-        <div>capital {country.capital}</div>
-        <div>population {country.population}</div>
-        <h3>languages</h3>
-        <ul>
-          {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
-        </ul>
-        <img alt="flag" width="100" src={country.flag}/>
-      </div>
+      <FullCountryInfo country={country}/>
     )
   }
+  
   return (
     <div>
       <ul>
-        {filtered.map(country => <li key={country.name}>{country.name}</li>)}
+        {filtered.map(country =>
+          <li key={country.name}>{country.name}
+            {/*mitenköhän input valuen saisi päivittymään samalla*/}
+            <button onClick={() => setFilter(country.name)}> show </button>
+          </li>
+        )}
       </ul>
     </div>    
   )
@@ -44,22 +55,23 @@ const ListCountries = ({ countries, filter }) => {
 const App = () => {
   const [filter, setFilter] = useState('')
   const [countries, setCountries] = useState([])
+
   useEffect(() => {
     axios
       .get(`https://restcountries.eu/rest/v2/all`)
       .then(response => {
         setCountries(response.data)
-      } )
+      })
   }, [])
 
   const handleInputChange = (event) => {
     setFilter(event.target.value)
   }
-
+  
   return (
     <div>
       <FindCountries value={filter} handleInputChange={handleInputChange} />
-      <ListCountries countries={countries} filter={filter}/>
+      <ListCountries countries={countries} filter={filter} setFilter={setFilter}/>
     </div>
   )
 }
