d6f46b659a408c20d53e81bf91deb936862e64a7
Persorkki <persorkki@protonmail.com>
2021-07-13 04:34:52 +0300

    2.14 maiden tiedot, step3, valmis mutta en tyytyväinen

--- osa2/maidentiedot/src/components/Weather.js
+++ osa2/maidentiedot/src/components/Weather.js
@@ -1,39 +1,39 @@
-
 import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 const Weather = ({ country }) => {
-    const [weather, setWeather] = useState({})
-    useEffect(() => {
-      axios
-        .get('http://api.weatherstack.com/current', {
-          params:
-          {
-            access_key: process.env.REACT_APP_API_KEY,
-            query: country.name,
-          }
-        })
-        .then((res) => {
-          const newObj = {
-            city: res.data.location.name,
-            temperature: res.data.current.temperature,
-            weather_icon: res.data.current.weather_icons[0],
-            wind_dir: res.data.current.wind_dir,
-            wind_speed: res.data.current.wind_speed,
-          }
-          setWeather(newObj)
-        })
-    }, [country])
-    
-    return (
-      <div>
-        <h3>weather in {weather.city}</h3>
-        <div><strong>temperature:</strong> {weather.temperature} celsius</div>
-        <div><img alt="weather icon" src={weather.weather_icon}/></div>
-        <div><strong>wind:</strong>{weather.wind_speed}mph direction {weather.wind_dir}</div>
-      </div>
-    )
-  }
+  //asetetaan alkuasetelmaksi toimiva muoto, jotta vältytään typeErrorilta kun data ei ole vielä saapunut perille
+  //toimiva ratkaisu voisi olla myös datan validointi ennen palautusta
+  const [weather, setWeather] = useState({
+    current: { temperature: 0, weather_icons: [], wind_speed: 0, wind_dir: "" },
+    location: { name: "" }
+  })
 
-  export default Weather
+  useEffect(() => {
+    axios
+      .get('http://api.weatherstack.com/current', {
+        params:
+        {
+          access_key: process.env.REACT_APP_API_KEY,
+          query: country.name,
+        }
+      })
+      .then((res) => {
+        const newObj = res.data
+        setWeather(newObj)
+      })
+  }, [country])
+
+  return (
+    <div>
+      <h3>weather in {weather.location.name}</h3>
+      <div><strong>temperature:</strong> {weather.current.temperature} celsius</div>
+      <div><img alt="weather icon" src={weather.current.weather_icons[0]} /></div>
+      <div><strong>wind:</strong>{weather.current.wind_speed}mph direction {weather.current.wind_dir}</div>
+    </div>
+  )
+
+}
+
+export default Weather
